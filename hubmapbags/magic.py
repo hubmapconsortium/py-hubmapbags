fc9c62bd832184063c96ea909f0b83dbdf9dcfa9
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2022-11-25 00:57:42 -0500

    Updated method

--- hubmapbags/magic.py
+++ hubmapbags/magic.py
@@ -305,7 +305,7 @@ def do_it( input, dbgap_study_id=None, \
 
 			if compute_uuids:
 				print('Generating UUIDs via the uuid-api')
-				if uuids.should_i_generate_uuids( hubmap_id=hubmap_id, \
+				if uuids.should_i_generate_uuids( hubmap_id, \
 					instance=instance, \
 					token=token, \
 					debug=debug):
