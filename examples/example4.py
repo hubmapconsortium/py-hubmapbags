c32762751779dae4c3d7a0add20dfe838fbaccfd
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2022-11-25 15:29:58 -0500

    Updated examples

--- examples/example4.py
+++ examples/example4.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 id = 'HBM666.FFFW.363'
 dbgap_study_id = 'phs002267'
-token='Aga5KV8d7n5xavgEMYV9GGWN8547xQ8mrOWbMkbGvlGo945rVNc9CxgXnga8YMpa5OgWz8zmPezPo6FnlYwVQSWG0z'
+token=''
 
 answer = hubmapbags.uuids.populate_local_file_with_remote_uuids( id, instance='prod', token=token, debug=True )
 print(answer)
