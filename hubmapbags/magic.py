41e0be6351cb59f0d075f8f4db2b7f5cc4585fce
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2023-01-02 07:52:07 -0500

    Updated methods

--- hubmapbags/magic.py
+++ hubmapbags/magic.py
@@ -97,10 +97,34 @@ def __get_number_of_files( output_directory ):
 	except:
 		return 0
 
+def __extract_datasets_from_input( input ):
+	'''
+	Helper function that returns a list of valid datasets (if any).
+	'''
+
+	if os.path.isfile( input ):
+		utilities.pprint('Extracting datasets from ' + input)
+		metadata_file = input
+		datasets = pd.read_csv( metadata_file, sep='\t' )
+
+		if datasets is None:
+			warnings.warn('No datasets found. Exiting process.')
+		else:
+			print( 'Number of datasets found is ' + str(datasets.shape[0]) )
+	else:
+		utilities.pprint('Processing dataset with HuBMAP ID ' + input)
+		datasets = __extract_dataset_info_from_db( input, token=token, instance=instance )
+
+		if datasets is None:
+			warnings.warn('No datasets found. Exiting process.')
+
+	return datasets
+
 def do_it( input, dbgap_study_id=None, \
 	 	compute_uuids=False, \
 		overwrite=False, \
 		copy_output_to=None, \
+		build_bags=False, \
 		token=None, \
 		instance='test', \
 		debug=True ):
@@ -113,6 +137,8 @@ def do_it( input, dbgap_study_id=None, \
 	:type dbgap_study_id: None or string
 	:param overwrite: If set to TRUE, then it will overwrite an existing pickle file associated with the HuBMAP ID
 	:type overwrite: boolean
+	:param build_bags: If set to TRUE, the it will build the big data bag. 
+	:type build_bags: boolean
 	:param copy_output_to: If set, then a copy of the dataframe and the big data bag will be copied to this location
 	:type copy_output_to: None or string
 	:param token: A token to access HuBMAP resources
@@ -126,21 +152,11 @@ def do_it( input, dbgap_study_id=None, \
 	:rtype: boolean
 	'''
 
-	if os.path.isfile( input ):
-		utilities.pprint('Extracting datasets from ' + input)
-		metadata_file = input
-		datasets = pd.read_csv( metadata_file, sep='\t' )
-	else:
-		utilities.pprint('Processing dataset with HuBMAP ID ' + input)
-		datasets = __extract_dataset_info_from_db( input, token=token, instance=instance )
+	datasets = __extract_datasets_from_input( input )
+	if datasets is None:
+		return False
 
-		if datasets is None:
-			warnings.warn('No datasets found. Exiting.')
-			return False
-
-	print( 'Number of datasets found is ' + str(datasets.shape[0]) )
-	for dataset in datasets.iterrows():
-		dataset = dataset[1]
+	for index, dataset in datasets.iterrows():
 		status = dataset['ds.status'].lower()
 		data_type = dataset['ds.data_types'].replace('[','').replace(']','').replace('\'','').lower()
 		data_provider = dataset['ds.group_name']
@@ -149,9 +165,11 @@ def do_it( input, dbgap_study_id=None, \
 		biosample_id = dataset['first_sample_id']
 		data_directory = dataset['full_path']
 		print('Preparing bag for dataset ' + data_directory )
+
 		computing = data_directory.replace('/','_').replace(' ','_') + '.computing'
 		done = '.' + data_directory.replace('/','_').replace(' ','_') + '.done'
 		broken = '.' + data_directory.replace('/','_').replace(' ','_') + '.broken'
+		
 		organ_shortcode = dataset['organ_type']
 		organ_id = dataset['organ_id']
 		donor_id = dataset['donor_id']
@@ -175,103 +193,114 @@ def do_it( input, dbgap_study_id=None, \
 				print('Dataset is not published. Aborting computation.')
 				return
 
-			print('Checking if output directory exists.')
-			output_directory = data_type + '-' + status + '-' + dataset['dataset_uuid']
+			if build_bags:
+				print('Checking if output directory exists.')
+				output_directory = data_type + '-' + status + '-' + dataset['dataset_uuid']
 
-			print('Creating output directory ' + output_directory + '.' )
-			if Path(output_directory).exists() and Path(output_directory).is_dir():
-					print('Output directory found. Removing old copy.')
-					rmtree(output_directory)
-					os.mkdir(output_directory)
+				print('Creating output directory ' + output_directory + '.' )
+				if Path(output_directory).exists() and Path(output_directory).is_dir():
+						print('Output directory found. Removing old copy.')
+						rmtree(output_directory)
+						os.mkdir(output_directory)
+				else:
+						print('Output directory does not exist. Creating directory.')
+						os.mkdir(output_directory)
+
+				print('Making file.tsv')
+				if not Path('.data').exists():
+					Path('.data').mkdir()
+				temp_file = '.data/' + data_directory.replace('/','_').replace(' ','_') + '.pkl'
+
+				if overwrite:
+					print('Removing precomputed checksums')
+					if Path(temp_file).exists():
+						Path(temp_file).unlink()
+
+				answer = files.create_manifest( project_id=data_provider, \
+					assay_type=data_type, \
+					directory=data_directory, \
+					output_directory=output_directory, \
+					dbgap_study_id=dbgap_study_id, \
+					token=token, \
+					dataset_hmid=hubmap_id, \
+					dataset_uuid=hubmap_uuid )
+
+				print('Making biosample.tsv')
+				biosample.create_manifest( biosample_id, data_provider, organ_shortcode, output_directory )
+
+				print('Making biosample_in_collection.tsv')
+				biosample_in_collection.create_manifest( biosample_id, hubmap_id, output_directory )
+
+				print('Making project.tsv')
+				projects.create_manifest( data_provider, output_directory )
+
+				print('Making project_in_project.tsv')
+				project_in_project.create_manifest( data_provider, output_directory )
+
+				print('Making biosample_from_subject.tsv')
+				biosample_from_subject.create_manifest( biosample_id, donor_id, output_directory )
+
+				print('Making ncbi_taxonomy.tsv')
+				ncbi_taxonomy.create_manifest( output_directory )
+
+				print('Making collection.tsv')
+				collection.create_manifest( hubmap_id, output_directory )
+
+				print('Making collection_defined_by_project.tsv')
+				collection_defined_by_project.create_manifest( hubmap_id, data_provider, output_directory )
+
+				print('Making file_describes_collection.tsv')
+				file_describes_collection.create_manifest( hubmap_id, data_directory, output_directory )
+
+				print('Making dcc.tsv')
+				primary_dcc_contact.create_manifest( output_directory )
+
+				print('Making id_namespace.tsv')
+				id_namespace.create_manifest( output_directory )
+
+				print('Making subject.tsv')
+				subject.create_manifest( data_provider, donor_id, output_directory )
+
+				print('Making subject_in_collection.tsv')
+				subject_in_collection.create_manifest( donor_id, hubmap_id, output_directory )
+
+				print('Making file_in_collection.tsv')
+				answer = file_in_collection.create_manifest( hubmap_id, data_directory, output_directory )
+
+				print('Creating empty files')
+				file_describes_subject.create_manifest( output_directory )
+				file_describes_biosample.create_manifest( output_directory )
+				anatomy.create_manifest( output_directory )
+				assay_type.create_manifest( output_directory )
+				biosample_disease.create_manifest( output_directory )
+				biosample_gene.create_manifest( output_directory )
+				biosample_substance.create_manifest( output_directory )
+				collection_anatomy.create_manifest( output_directory )
+				collection_compound.create_manifest( output_directory )
+				collection_disease.create_manifest( output_directory )
+				collection_gene.create_manifest( output_directory )
+				collection_in_collection.create_manifest( output_directory )
+				collection_phenotype.create_manifest( output_directory )
+				collection_protein.create_manifest( output_directory )
+				collection_substance.create_manifest( output_directory )
+				collection_taxonomy.create_manifest( output_directory )
+				file_format.create_manifest( output_directory )
+				ncbi_taxonomy.create_manifest( output_directory )
+				subject_disease.create_manifest( output_directory )
+				subject_phenotype.create_manifest( output_directory )
+				subject_race.create_manifest( output_directory )
+				subject_role_taxonomy.create_manifest( output_directory )
+				subject_substance.create_manifest( output_directory )
+				file_format.create_manifest( output_directory )
 			else:
-					print('Output directory does not exist. Creating directory.')
-					os.mkdir(output_directory)
-
-			print('Making file.tsv')
-			if not Path('.data').exists():
-				Path('.data').mkdir()
-			temp_file = '.data/' + data_directory.replace('/','_').replace(' ','_') + '.pkl'
-
-			if overwrite:
-				print('Removing precomputed checksums')
-				if Path(temp_file).exists():
-					Path(temp_file).unlink()
-			answer = files.create_manifest( project_id=data_provider, \
-				assay_type=data_type, \
-				directory=data_directory, \
-				output_directory=output_directory, \
-				dbgap_study_id=dbgap_study_id, \
-				token=token, \
-				dataset_hmid=hubmap_id, \
-				dataset_uuid=hubmap_uuid )
-
-			print('Making biosample.tsv')
-			biosample.create_manifest( biosample_id, data_provider, organ_shortcode, output_directory )
-
-			print('Making biosample_in_collection.tsv')
-			biosample_in_collection.create_manifest( biosample_id, hubmap_id, output_directory )
-
-			print('Making project.tsv')
-			projects.create_manifest( data_provider, output_directory )
-
-			print('Making project_in_project.tsv')
-			project_in_project.create_manifest( data_provider, output_directory )
-
-			print('Making biosample_from_subject.tsv')
-			biosample_from_subject.create_manifest( biosample_id, donor_id, output_directory )
-
-			print('Making ncbi_taxonomy.tsv')
-			ncbi_taxonomy.create_manifest( output_directory )
-
-			print('Making collection.tsv')
-			collection.create_manifest( hubmap_id, output_directory )
-
-			print('Making collection_defined_by_project.tsv')
-			collection_defined_by_project.create_manifest( hubmap_id, data_provider, output_directory )
-
-			print('Making file_describes_collection.tsv')
-			file_describes_collection.create_manifest( hubmap_id, data_directory, output_directory )
-
-			print('Making dcc.tsv')
-			primary_dcc_contact.create_manifest( output_directory )
-
-			print('Making id_namespace.tsv')
-			id_namespace.create_manifest( output_directory )
-
-			print('Making subject.tsv')
-			subject.create_manifest( data_provider, donor_id, output_directory )
-
-			print('Making subject_in_collection.tsv')
-			subject_in_collection.create_manifest( donor_id, hubmap_id, output_directory )
-
-			print('Making file_in_collection.tsv')
-			answer = file_in_collection.create_manifest( hubmap_id, data_directory, output_directory )
-
-			print('Creating empty files')
-			file_describes_subject.create_manifest( output_directory )
-			file_describes_biosample.create_manifest( output_directory )
-			anatomy.create_manifest( output_directory )
-			assay_type.create_manifest( output_directory )
-			biosample_disease.create_manifest( output_directory )
-			biosample_gene.create_manifest( output_directory )
-			biosample_substance.create_manifest( output_directory )
-			collection_anatomy.create_manifest( output_directory )
-			collection_compound.create_manifest( output_directory )
-			collection_disease.create_manifest( output_directory )
-			collection_gene.create_manifest( output_directory )
-			collection_in_collection.create_manifest( output_directory )
-			collection_phenotype.create_manifest( output_directory )
-			collection_protein.create_manifest( output_directory )
-			collection_substance.create_manifest( output_directory )
-			collection_taxonomy.create_manifest( output_directory )
-			file_format.create_manifest( output_directory )
-			ncbi_taxonomy.create_manifest( output_directory )
-			subject_disease.create_manifest( output_directory )
-			subject_phenotype.create_manifest( output_directory )
-			subject_race.create_manifest( output_directory )
-			subject_role_taxonomy.create_manifest( output_directory )
-			subject_substance.create_manifest( output_directory )
-			file_format.create_manifest( output_directory )
+				answer = files.create_manifest( project_id=data_provider, \
+					assay_type=data_type, \
+					directory=data_directory, \
+					output_directory=output_directory, \
+					dbgap_study_id=dbgap_study_id, \
+					token=token, \
+					dataset_hmid=hubmap_id, \
+					dataset_uuid=hubmap_uuid )
 
 			print('Removing checkpoint ' + computing )
 			Path(computing).unlink()
@@ -306,19 +335,8 @@ def do_it( input, dbgap_study_id=None, \
 					warnings.warn('Output directory ' + copy_output_to + ' does not exist. Not copying results to destination.')
 
 			if compute_uuids:
-				print('Generating UUIDs via the uuid-api')
-				if uuids.should_i_generate_uuids( hubmap_id, \
-					instance=instance, \
-					token=token, \
-					debug=debug):
-					print('UUIDs not found in uuid-api database. Generating UUIDs.')
-					uuids.generate( temp_file, debug=debug )
-				else:
-					print('UUIDs found in uuid-api database. Populating local file')
-					uuids.populate_local_file_with_remote_uuids( hubmap_id, \
-						instance=instance, \
-						token=token, \
-						debug=debug )
+				print('Generating UUIDs via the UUID-API')
+				uuids.generate( temp_file, debug=debug )
 			if debug:
 				df=pd.read_pickle( temp_file )
 				df.to_csv(temp_file.replace('pkl','tsv'), sep="\t")
