19ca0e531b463cafd56012c243c55e9cbaa35478
Ivan Cao-Berg <icaoberg@psc.edu>
2023-05-02 22:10:22 -0400

    Added new function to handle error messages and optimized loop for performance.

--- hubmapbags/reports.py
+++ hubmapbags/reports.py
@@ -64,55 +64,61 @@ def daily(token: str, ncores=16) -> pd.DataFrame:
     Creates daily report and returns a helpful dataframe
     """
 
-    pandarallel.initialize(progress_bar=True, nb_workers=ncores)
-
-    utilities.pprint("Getting list of assay types")
-    assay_types = apis.get_assay_types(token=token)
-
-    print("Getting list of dataset IDs")
-    datasets = []
-    for assay_type in tqdm(assay_types):
-        hubmap_ids = apis.get_ids(assay_type, token=token)
-        datasets.extend(hubmap_ids)
-
-    df = pd.DataFrame(datasets)
-
-    utilities.pprint("Getting group name")
-    df["group_name"] = df["hubmap_id"].parallel_apply(__get_group_name, token=token)
-
-    utilities.pprint("Getting data type")
-    df["data_type"] = df["hubmap_id"].parallel_apply(__get_data_type, token=token)
-
-    utilities.pprint("Get dataset type")
-    df["dataset_type"] = df["hubmap_id"].parallel_apply(__get_dataset_type, token=token)
-
-    utilities.pprint("Getting creation timestamp")
-    df["created_datetime"] = df["hubmap_id"].parallel_apply(
-        __get_created_timestamp, token=token
-    )
-
-    utilities.pprint("Getting published timestamp")
-    df["published_datetime"] = df["hubmap_id"].parallel_apply(
-        __get_published_timestamp, token=token
-    )
-
-    utilities.pprint("Getting protected status")
-    df["is_protected"] = df["hubmap_id"].parallel_apply(__is_protected, token=token)
-
-    print("Sorting dataframe")
-    df = df.sort_values("published_datetime", ascending=False)
-
-    report_output_directory = "daily-report"
-    if not Path(report_output_directory).exists():
-        Path(report_output_directory).mkdir()
-
     now = datetime.now()
+    report_output_directory = "daily-report"
     report_output_filename = (
-        report_output_directory + "/" + str(now.strftime("%Y%m%d")) + ".tsv"
+        f'{report_output_directory}/{str(now.strftime("%Y%m%d"))}.tsv'
     )
 
-    try:
-        df.to_csv(report_output_filename, sep="\t", index=False)
-    except:
-        print(f"Unable to save dataframe to {report_output_filename}.")
-    return df
+    if Path(report_output_filename):
+        df = pd.read_csv(report_output_filename, sep="\t")
+        return df
+    else:
+        pandarallel.initialize(progress_bar=True, nb_workers=ncores)
+
+        utilities.pprint("Getting list of assay types")
+        assay_types = apis.get_assay_types(token=token)
+
+        print("Getting list of dataset IDs")
+        datasets = []
+        for assay_type in tqdm(assay_types):
+            hubmap_ids = apis.get_ids(assay_type, token=token)
+            datasets.extend(hubmap_ids)
+
+        df = pd.DataFrame(datasets)
+
+        utilities.pprint("Getting group name")
+        df["group_name"] = df["hubmap_id"].parallel_apply(__get_group_name, token=token)
+
+        utilities.pprint("Getting data type")
+        df["data_type"] = df["hubmap_id"].parallel_apply(__get_data_type, token=token)
+
+        utilities.pprint("Get dataset type")
+        df["dataset_type"] = df["hubmap_id"].parallel_apply(
+            __get_dataset_type, token=token
+        )
+
+        utilities.pprint("Getting creation timestamp")
+        df["created_datetime"] = df["hubmap_id"].parallel_apply(
+            __get_created_timestamp, token=token
+        )
+
+        utilities.pprint("Getting published timestamp")
+        df["published_datetime"] = df["hubmap_id"].parallel_apply(
+            __get_published_timestamp, token=token
+        )
+
+        utilities.pprint("Getting protected status")
+        df["is_protected"] = df["hubmap_id"].parallel_apply(__is_protected, token=token)
+
+        print("Sorting dataframe")
+        df = df.sort_values("published_datetime", ascending=False)
+
+        if not Path(report_output_directory).exists():
+            Path(report_output_directory).mkdir()
+
+        try:
+            df.to_csv(report_output_filename, sep="\t", index=False)
+        except:
+            print(f"Unable to save dataframe to {report_output_filename}.")
+        return df
