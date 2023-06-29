bfcc44f6045ac8de11a2e1c49103e50a67d13d04
Ivan Cao-Berg <icaoberg@psc.edu>
2023-06-29 16:57:50 -0400

    # This code change adds a new plot that shows the count of groups by their status and data type. # The code also adds a new plot that shows the count of data types by their status and group name. # Also, the code adds a new plot that shows the cumulative sum of counts by published datetime. # Finally, the code modifies the plot size and saves the plot with a new filename.

--- hubmapbags/plots.py
+++ hubmapbags/plots.py
@@ -53,101 +53,6 @@ def by_data_type(df: pd.DataFrame) -> None:
 
 
 def by_group(df: pd.DataFrame) -> None:
-    df = df[df["status"] == "Published"]
-
-    now = datetime.now()
-    plt.rcParams["figure.figsize"] = [50.0, 50.0]
-    plt.rcParams["figure.dpi"] = 500
-
-    group = df.groupby(["group_name", "status"]).count()[["data_type"]]
-
-    # First plot
-    plt.figure(figsize=(30, 35))
-    g = sns.displot(
-        df[df["dataset_type"] == "Primary"],
-        height=10,
-        x="group_name",
-        hue="status",
-        multiple="stack",
-        aspect=1.5,
-        log_scale=(False, True),
-    )
-    plt.xticks(rotation=45, fontsize=10, ha="right")
-    g.set(xlabel="Groups", ylabel="Count", title=str(now.strftime("%Y%m%d")))
-    report_output_directory = "daily-report"
-    Path(report_output_directory).mkdir(exist_ok=True)
-    report_output_filename = (
-        f'{report_output_directory}/groups-{str(now.strftime("%Y%m%d"))}.png'
-    )
-    plt.savefig(report_output_filename)
-    plt.tight_layout()
-    plt.savefig("plot.png")
-    plt.close()
-
-    # Second plot
-    plt.figure(figsize=(50.0, 50.0))
-    g = sns.displot(
-        df[df["dataset_type"] == "Primary"],
-        x="status",
-        height=12,
-        hue="data_type",
-        multiple="stack",
-        aspect=1,
-        palette="hls",
-    )
-    plt.xticks(
-        df[df["dataset_type"] == "Primary"]["status"],
-        df[df["dataset_type"] == "Primary"]["status"],
-        rotation=45,
-        fontsize=10,
-    )
-    g.set(xlabel="Data Type", ylabel="Count", title=str(now.strftime("%Y%m%d")))
-    sns.move_legend(g, "upper right", ncol=4, title="Data Type", frameon=False)
-    plt.tight_layout()
-    plt.savefig("plot.png")
-    plt.close()
-
-    # Third plot
-    plt.figure(figsize=(50.0, 50.0))
-    g = sns.displot(
-        df[df["dataset_type"] == "Primary"],
-        height=10,
-        x="data_type",
-        hue="status",
-        multiple="stack",
-        aspect=2,
-    )
-    plt.xticks(
-        df[df["dataset_type"] == "Primary"]["data_type"],
-        df[df["dataset_type"] == "Primary"]["data_type"],
-        rotation=45,
-        fontsize=10,
-        ha="right",
-    )
-    g.set(xlabel="Data Type", ylabel="Count", title=str(now.strftime("%Y%m%d")))
-    sns.move_legend(g, "center right", ncol=1, title="Dataset status", frameon=False)
-    plt.tight_layout()
-    plt.show()
-
-
-def by_date(df: pd.DataFrame) -> None:
-    df = df[df["status"] == "Published"]
-
-    now = datetime.now()
-    plt.rcParams["figure.figsize"] = [15.0, 15.0]
-    plt.rcParams["figure.dpi"] = 100
-
-    df = df.groupby("published_datetime").count()
-    df = df[["uuid"]]
-    df = df.rename({"uuid": "counts"}, axis=1)
-    df["cumsum"] = df["counts"].cumsum()
-
-    g = sns.pointplot(x=df.index, y="cumsum", data=df)
-    plt.xticks(rotation="vertical")
-    g.set(xlabel="Dates", ylabel="Cumulative sum", title=str(now.strftime("%Y%m%d")))
-    plt.tight_layout()
-
-    # get daily report
     try:
         report_output_directory = "daily-report"
         if not Path(report_output_directory).exists():
@@ -155,8 +60,26 @@ def by_date(df: pd.DataFrame) -> None:
 
         now = datetime.now()
         report_output_filename = (
-            f'{report_output_directory}/date-{str(now.strftime("%Y%m%d"))}.png'
+            f'{report_output_directory}/group-{str(now.strftime("%Y%m%d"))}.png'
         )
+
+        fig = plt.gcf()
+        fig.set_size_inches(30, 35)
+
+        g = sns.displot(
+            df[df["dataset_type"] == "Primary"],
+            height=10,
+            x="group_name",
+            hue="status",
+            multiple="stack",
+            aspect=1.5,
+            log_scale=(False, True),
+        )  # Apply log scale on y-axis
+        plt.xticks(rotation=45, fontsize=10, ha="right")
+
+        g.set(xlabel="Groups", ylabel="Count", title=str(now.strftime("%Y%m%d")))
+
+        plt.tight_layout()
         plt.savefig(report_output_filename)
     except:
         print(f"Unable to save plot to {report_output_filename}.")
@@ -165,3 +88,7 @@ def by_date(df: pd.DataFrame) -> None:
         plt.show()
     except:
         print("Unable to display plot.")
+
+
+def by_date(df: pd.DataFrame) -> None:
+    raise NotImplementedError()
