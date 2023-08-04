e9486ac865e3b1597f9f34cb3bf547efb90a758e
Ivan Cao-Berg <icaoberg@psc.edu>
2023-08-04 06:20:08 -0400

    Added docstrings

--- hubmapbags/reports.py
+++ hubmapbags/reports.py
@@ -1,13 +1,40 @@
 from datetime import datetime
 from pathlib import Path
+
 import pandas as pd
 from pandarallel import pandarallel
 from tqdm import tqdm
 
-from . import apis, utilities, plots
+from . import apis, plots, utilities
 
 
 def __is_protected(hubmap_id, token=None):
+    """
+    Determine whether a given HuBMAP ID corresponds to a dataset containing human genetic sequences.
+
+    This function calls the `get_dataset_info` function from the `apis` module to fetch the dataset metadata and
+    subsequently extracts the information indicating if the dataset contains human genetic sequences. If an error
+    occurs during the retrieval process or the information is not found, the function returns None.
+
+    :param hubmap_id: The HuBMAP ID for which the protection status is to be checked.
+    :type hubmap_id: str
+
+    :param token: Authorization token to access the HuBMAP API. Default is None.
+    :type token: str, optional
+
+    :return: True if the dataset associated with the HuBMAP ID contains human genetic sequences, False otherwise.
+             Returns None if the information cannot be retrieved.
+
+    .. note::
+       - The `apis.get_dataset_info` function should be properly implemented and imported.
+       - This function is designed as a private helper function for the primary `daily` report generation function.
+
+    .. warning::
+       - Ensure that a valid token is provided if required by the HuBMAP API.
+       - The "contains_human_genetic_sequences" key is assumed to be present in the metadata returned by the HuBMAP API.
+
+    """
+
     try:
         metadata = apis.get_dataset_info(
             hubmap_id, instance="prod", token=token, overwrite=False
@@ -18,6 +45,31 @@ def __is_protected(hubmap_id, token=None):
 
 
 def __get_published_timestamp(hubmap_id, token=None):
+    """
+    Retrieve the publication timestamp associated with a given HuBMAP ID.
+
+    This function calls the `get_dataset_info` function from the `apis` module to fetch the dataset metadata and
+    subsequently extracts and converts the publication timestamp. If an error occurs during the retrieval process or
+    the timestamp is not found, the function returns None.
+
+    :param hubmap_id: The HuBMAP ID for which the publication timestamp is to be retrieved.
+    :type hubmap_id: str
+
+    :param token: Authorization token to access the HuBMAP API. Default is None.
+    :type token: str, optional
+
+    :return: The datetime representation of the publication timestamp associated with the HuBMAP ID if found, otherwise None.
+
+    .. note::
+       - The `apis.get_dataset_info` function should be properly implemented and imported.
+       - This function is designed as a private helper function for the primary `daily` report generation function.
+
+    .. warning::
+       - Ensure that a valid token is provided if required by the HuBMAP API.
+       - The timestamp returned by the HuBMAP API is assumed to be in milliseconds since the Unix epoch.
+
+    """
+
     try:
         metadata = apis.get_dataset_info(
             hubmap_id, instance="prod", token=token, overwrite=False
@@ -28,6 +80,31 @@ def __get_published_timestamp(hubmap_id, token=None):
 
 
 def __get_created_timestamp(hubmap_id, token=None):
+    """
+    Retrieve the creation timestamp associated with a given HuBMAP ID.
+
+    This function calls the `get_dataset_info` function from the `apis` module to fetch the dataset metadata and
+    subsequently extracts and converts the creation timestamp. If an error occurs during the retrieval process or
+    the timestamp is not found, the function returns None.
+
+    :param hubmap_id: The HuBMAP ID for which the creation timestamp is to be retrieved.
+    :type hubmap_id: str
+
+    :param token: Authorization token to access the HuBMAP API. Default is None.
+    :type token: str, optional
+
+    :return: The datetime representation of the creation timestamp associated with the HuBMAP ID if found, otherwise None.
+
+    .. note::
+       - The `apis.get_dataset_info` function should be properly implemented and imported.
+       - This function is designed as a private helper function for the primary `daily` report generation function.
+
+    .. warning::
+       - Ensure that a valid token is provided if required by the HuBMAP API.
+       - The timestamp returned by the HuBMAP API is assumed to be in milliseconds since the Unix epoch.
+
+    """
+
     try:
         metadata = apis.get_dataset_info(
             hubmap_id, instance="prod", token=token, overwrite=False
@@ -38,6 +115,30 @@ def __get_created_timestamp(hubmap_id, token=None):
 
 
 def __get_group_name(hubmap_id, token=None):
+    """
+    Retrieve the group name associated with a given HuBMAP ID.
+
+    This function calls the `get_dataset_info` function from the `apis` module to fetch the dataset metadata and
+    subsequently extracts the group name. If an error occurs during the retrieval process or the group name is not found,
+    the function returns None.
+
+    :param hubmap_id: The HuBMAP ID for which the group name is to be retrieved.
+    :type hubmap_id: str
+
+    :param token: Authorization token to access the HuBMAP API. Default is None.
+    :type token: str, optional
+
+    :return: The group name associated with the HuBMAP ID if found, otherwise None.
+
+    .. note::
+       - The `apis.get_dataset_info` function should be properly implemented and imported.
+       - This function is designed as a private helper function for the primary `daily` report generation function.
+
+    .. warning::
+       - Ensure that a valid token is provided if required by the HuBMAP API.
+
+    """
+
     try:
         metadata = apis.get_dataset_info(
             hubmap_id, instance="prod", token=token, overwrite=False
@@ -48,6 +149,30 @@ def __get_group_name(hubmap_id, token=None):
 
 
 def __get_data_type(hubmap_id, token=None):
+    """
+    Retrieve the data type associated with a given HuBMAP ID.
+
+    This function calls the `get_dataset_info` function from the `apis` module to fetch the dataset metadata and
+    subsequently extracts the data type. If an error occurs during the retrieval process or the data type is not found,
+    the function returns None.
+
+    :param hubmap_id: The HuBMAP ID for which the data type is to be retrieved.
+    :type hubmap_id: str
+
+    :param token: Authorization token to access the HuBMAP API. Default is None.
+    :type token: str, optional
+
+    :return: The data type associated with the HuBMAP ID if found, otherwise None.
+
+    .. note::
+       - The `apis.get_dataset_info` function should be properly implemented and imported.
+       - This function is designed as a private helper function for the primary `daily` report generation function.
+
+    .. warning::
+       - Ensure that a valid token is provided if required by the HuBMAP API.
+
+    """
+
     try:
         metadata = apis.get_dataset_info(
             hubmap_id, instance="prod", token=token, overwrite=False
@@ -58,6 +183,29 @@ def __get_data_type(hubmap_id, token=None):
 
 
 def __get_dataset_type(hubmap_id, token=None):
+    """
+    Retrieve the dataset type for a given HuBMAP ID.
+
+    This function calls the `get_dataset_type` function from the `apis` module to fetch the dataset type.
+    If an error occurs during the retrieval process, the function returns None.
+
+    :param hubmap_id: The HuBMAP ID for which the dataset type is to be retrieved.
+    :type hubmap_id: str
+
+    :param token: Authorization token to access the HuBMAP API. Default is None.
+    :type token: str, optional
+
+    :return: The dataset type if found, otherwise None.
+
+    .. note::
+       - The `apis.get_dataset_type` function should be properly implemented and imported.
+       - This function is designed as a private helper function for the primary `daily` report generation function.
+
+    .. warning::
+       - Ensure that a valid token is provided if required by the HuBMAP API.
+
+    """
+
     try:
         return apis.get_dataset_type(
             hubmap_id, instance="prod", token=token, overwrite=False
@@ -68,7 +216,32 @@ def __get_dataset_type(hubmap_id, token=None):
 
 def daily(token: str, ncores=16) -> pd.DataFrame:
     """
-    Creates daily report and returns a helpful dataframe
+    Generate a daily report of datasets with details like group name, data type, creation timestamp,
+    and more, for given assay types.
+
+    This function fetches datasets associated with each assay type and compiles a dataframe with various
+    details. The function then sorts the dataframe based on the published date and saves the output in
+    a TSV (tab-separated values) format. Additionally, the function generates a plot based on group distributions.
+
+    :param token: Authorization token to access the HuBMAP API.
+    :type token: str
+
+    :param ncores: Number of cores to be used for parallel processing. Default is 16.
+    :type ncores: int, optional
+
+    :return: A pandas DataFrame containing details for each dataset, sorted by published date.
+
+    .. note::
+       - The report is saved in the `daily-report` directory with the filename format YYYYMMDD.tsv
+         (e.g., 20230804.tsv for August 4, 2023).
+       - If the report for the current date exists, the function loads the report from the file
+         instead of re-fetching all the data.
+
+    .. warning::
+       - Ensure that a valid token is provided to access the HuBMAP API.
+       - The private helper functions (`__get_group_name`, `__get_data_type`, etc.)
+         are assumed to exist and work properly.
+
     """
 
     now = datetime.now()
