c6f65bf6b5bf12aa25d51ded1e88ac8696f58d79
j-uranic <117292295+j-uranic@users.noreply.github.com>
2023-08-17 11:47:19 -0400

    Create protein_gene.py
    Added to match C2M2 Table Guide

---
+++ hubmapbags/protein_gene.py
@@ -0,0 +1,26 @@
+import os
+
+import pandas as pd
+
+
+def _build_dataframe() -> pd.DataFrame:
+    """
+    Build a dataframe with minimal information for this entity.
+    """
+
+    headers = ["protein", "gene"]
+
+    df = pd.DataFrame(columns=headers)
+
+    return df
+
+
+def create_manifest(output_directory: str) -> bool:
+    try:
+        filename = os.path.join(output_directory, "protein_gene.tsv")
+        df = _build_dataframe()
+        df.to_csv(filename, sep="\t", index=False)
+
+        return True
+    except:
+        return False
