f553b9553bd25b9a61efc5b44883496f3d88805b
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2023-09-08 16:35:01 -0400

    Removed double entry

--- hubmapbags/file.py
+++ hubmapbags/file.py
@@ -218,7 +218,6 @@ def __get_assay_type_from_obi(assay_type: str) -> str:
     assay["snrnaseq-10xgenomics-v3"] = "OBI:0003109"
     assay["mibi"] = "OBI:0003100"
     assay["cell-dive"] = "OBI:0003092"
-    assay["cell-dive"] = "OBI:0003092"
     assay["maldi-ims"] = "OBI:000309"
     assay["maldi-ims-neg"] = "OBI:000309"
     assay["maldi-ims-pos"] = "OBI:000309"
