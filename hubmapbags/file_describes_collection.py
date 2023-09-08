cb5b4d659ce8b1c593d1bd8951b2769ab7b6eb1b
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2023-09-07 20:12:19 -0400

    Added call to python-hubmap-inventory simplifying computation

--- hubmapbags/file_describes_collection.py
+++ hubmapbags/file_describes_collection.py
@@ -19,30 +19,24 @@ def _build_dataframe(hubmap_id: str, hubmap_uuid: str, directory: str) -> pd.Dat
         "collection_local_id",
     ]
 
-    if not Path(".data").exists():
-        Path(".data").mkdir()
+    df = hubmapinventory.get(hubmap_id=hubmap_id, token=token)
+    print(f"Temporary file {temp_file} found. Loading df into memory.")
+    df = pd.read_csv(temp_file, sep="\t")
 
-    temp_file = f".data/{hubmap_uuid}.tsv"
-    if Path(temp_file).exists():
-        print("Temporary file " + temp_file + " found. Loading df into memory.")
-        df = pd.read_csv(temp_file, sep="\t")
-
-        df = df[df["filename"].str.contains("metadata.tsv")]
-        df["subject_id_namespace"] = id_namespace
-        df["file_id_namespace"] = id_namespace
-        df = df.rename(columns={"file_uuid": "file_local_id"}, errors="raise")
-        df["collection_local_id"] = hubmap_id
-        df["collection_id_namespace"] = id_namespace
-        df = df[
-            [
-                "file_id_namespace",
-                "file_local_id",
-                "collection_id_namespace",
-                "collection_local_id",
-            ]
+    df = df[df["filename"].str.contains("metadata.tsv")]
+    df["subject_id_namespace"] = id_namespace
+    df["file_id_namespace"] = id_namespace
+    df = df.rename(columns={"file_uuid": "file_local_id"}, errors="raise")
+    df["collection_local_id"] = hubmap_id
+    df["collection_id_namespace"] = id_namespace
+    df = df[
+        [
+            "file_id_namespace",
+            "file_local_id",
+            "collection_id_namespace",
+            "collection_local_id",
         ]
-    else:
-        df = []
+    ]
 
     return df
 
@@ -51,18 +45,7 @@ def create_manifest(
     hubmap_id: str, hubmap_uuid: str, directory: str, output_directory: str
 ) -> bool:
     filename = os.path.join(output_directory, "file_describes_collection.tsv")
+    df = _build_dataframe(hubmap_id, hubmap_uuid, directory)
+    df.to_csv(filename, sep="\t", index=False)
 
-    if not Path(".data").exists():
-        Path(".data").mkdir()
-
-    temp_file = f".data/{hubmap_uuid}.tsv"
-    if not Path(directory).exists() and not Path(temp_file).exists():
-        print(
-            f"Data directory {directory} does not exist. Temp file was not found either."
-        )
-        return False
-    else:
-        df = _build_dataframe(hubmap_id, hubmap_uuid, directory)
-        df.to_csv(filename, sep="\t", index=False)
-
-        return True
+    return True
