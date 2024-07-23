85d69f1f624580f8914001a6e9958abd0ddfdbda
icaoberg <icaoberg@andrew.cmu.edu>
2024-07-23 20:56:21 +0000

    Fixed typo

--- hubmapbags/tag.py
+++ hubmapbags/tag.py
@@ -16,7 +16,7 @@ def _missing_contributors_metadata_file(metadata):
 
 def _is_dataset_directory_empty(metadata):
     if metadata['entity_type'] == 'Dataset':
-        directory = f'/hive/hubmap/data/{metadata['local_directory_rel_path']}'
+        directory = f'/hive/hubmap/data/{metadata["local_directory_rel_path"]}'
         if Path(directory).exists():
             # List all files including hidden files (using .glob('**/*'))
             files = list(directory_path.glob('**/*'))
