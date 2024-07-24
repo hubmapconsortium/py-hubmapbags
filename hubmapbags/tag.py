bfa8b02921a282f255b32c56927c10e4637ac618
icaoberg <icaoberg@andrew.cmu.edu>
2024-07-24 02:17:52 +0000

    Documented more

--- hubmapbags/tag.py
+++ hubmapbags/tag.py
@@ -182,6 +182,41 @@ def _is_dataset_directory_empty(metadata):
 
 
 def _is_contributors_metadata_file_empty(metadata):
+    """
+    Checks if the contributors metadata file is empty based on the metadata provided.
+
+    Parameters:
+    metadata (dict): A dictionary containing metadata information. It should include the necessary keys to locate the contributors file.
+
+    Returns:
+    bool or None:
+        - `True` if the contributors file exists and is empty.
+        - `False` if the contributors file exists and is not empty.
+        - `None` if the contributors file does not exist or is not specified in the metadata.
+
+    Example:
+    >>> metadata = {
+    ...     "ingest_metadata": {
+    ...         "metadata": {
+    ...             "contributors_path": "contributors/contributors.txt"
+    ...         }
+    ...     },
+    ...     "local_directory_rel_path": "protected/example/path",
+    ...     "uuid": "1234-5678"
+    ... }
+    >>> _is_contributors_metadata_file_empty(metadata)
+    True  # Assuming the file exists and is empty
+
+    >>> metadata = {
+    ...     "ingest_metadata": {
+    ...         "metadata": {}
+    ...     },
+    ...     "local_directory_rel_path": "example/path",
+    ...     "uuid": "1234-5678"
+    ... }
+    >>> _is_contributors_metadata_file_empty(metadata)
+    None
+    """
     file = __get_contributors_file(metadata)
     if file is not None and Path(file).exists():
         if Path(file).stat().st_size == 0:
@@ -198,7 +233,26 @@ def _has_registration_metadata(metadata):
 
 
 def _has_doi_url(metadata):
+    """
+    Checks if the metadata contains a DOI (Digital Object Identifier) URL.
+
+    Parameters:
+    metadata (dict): A dictionary containing metadata information.
+
+    Returns:
+    bool: True if the metadata contains a "doi_url" key, otherwise False.
+
+    Example:
+    >>> metadata = {"doi_url": "https://doi.org/10.1234/example"}
+    >>> _has_doi_url(metadata)
+    True
+
+    >>> metadata = {"title": "A Sample Paper"}
+    >>> _has_doi_url(metadata)
+    False
+    """
     return "doi_url" in metadata
+
 
 
 def _missing_instrument_metadata_file(metadata):
@@ -212,6 +266,30 @@ def _has_orcid_contributor_metadata(metadata):
 
 
 def _has_empty_directories(metadata):
+    """
+    Checks if the directory specified in the metadata contains any empty subdirectories.
+
+    Parameters:
+    metadata (dict): A dictionary containing metadata information. It should include the necessary information to construct the directory path.
+
+    Returns:
+    bool: True if there are empty subdirectories, otherwise False.
+
+    Example:
+    >>> metadata = {
+    ...     "local_directory_rel_path": "example/path",
+    ...     "uuid": "1234-5678"
+    ... }
+    >>> _has_empty_directories(metadata)
+    True  # Assuming there are empty subdirectories in the specified path
+
+    >>> metadata = {
+    ...     "local_directory_rel_path": "example/path",
+    ...     "uuid": "1234-5678"
+    ... }
+    >>> _has_empty_directories(metadata)
+    False  # Assuming there are no empty subdirectories in the specified path
+    """
     directory = Path(__get_directory(metadata))
     if (
         len(
@@ -226,6 +304,7 @@ def _has_empty_directories(metadata):
         return True
     else:
         return False
+
 
 
 def _instrument_metadata_file_is_empty(metadata):
@@ -285,8 +364,32 @@ def __missing_description_field_in_the_portal(metadata):
     return None
 
 
-def dataset(hubmap_id, token=None, debug=False):
-    """Retrieve dataset information and generate tags based on metadata."""
+def get(hubmap_id, token=None, debug=False):
+    """
+    Retrieve dataset information and generate tags based on metadata.
+
+    Parameters:
+    hubmap_id (str): The unique identifier for the dataset within the HuBMAP consortium.
+    token (str, optional): An optional authentication token for accessing the dataset information. Defaults to None.
+    debug (bool, optional): A flag for enabling debug mode, which provides additional print statements for debugging. Defaults to False.
+
+    Returns:
+    dict: A dictionary containing various checks and their corresponding statuses based on the dataset's metadata.
+
+    Example:
+    >>> hubmap_id = "1234-5678"
+    >>> dataset_info = dataset(hubmap_id)
+    >>> print(dataset_info)
+    {
+        'hubmap_id': '1234-5678',
+        'uuid': 'abcd-efgh-ijkl-mnop',
+        'status': 'Published',
+        'dataset_type': 'RNA-Seq',
+        'missing_description_field_in_the_portal': False,
+        'is_flagged_for_deletion': False,
+        ...
+    }
+    """
     metadata = get_dataset_info(hubmap_id, token=token)
     if not metadata:
         print("Unable to retrieve metadata.")
@@ -297,43 +400,27 @@ def dataset(hubmap_id, token=None, debug=False):
         "uuid": metadata["uuid"],
         "status": metadata["status"],
         "dataset_type": metadata["dataset_type"],
-        "missing_description_field_in_the_portal": __missing_description_field_in_the_portal(
-            metadata
-        ),
+        "missing_description_field_in_the_portal": __missing_description_field_in_the_portal(metadata),
         "is_flagged_for_deletion": __is_flagged_for_deletion(metadata),
-        "failed_to_produce_derived_dataset": __failed_to_produce_derived_dataset(
-            metadata
-        ),
+        "failed_to_produce_derived_dataset": __failed_to_produce_derived_dataset(metadata),
         "is_awaiting_processing_pipeline": __is_awaiting_processing_pipeline(metadata),
         "is_awating_review": __is_awating_review(metadata),
         "unrecognized_provenance": __unrecognized_provenance(metadata),
-        "has_nonstandard_filenames_extensions": __has_nonstandard_filenames_extensions(
-            metadata
-        ),
-        "is_nonstandard_directory_structure": __is_nonstandard_directory_structure(
-            metadata
-        ),
+        "has_nonstandard_filenames_extensions": __has_nonstandard_filenames_extensions(metadata),
+        "is_nonstandard_directory_structure": __is_nonstandard_directory_structure(metadata),
         "extra_datasets_in_upload": _extra_datasets_in_upload(metadata),
-        "missing_dataset_files_in_metadata": _missing_dataset_files_in_metadata(
-            metadata
-        ),
+        "missing_dataset_files_in_metadata": _missing_dataset_files_in_metadata(metadata),
         "assay_specific_files_are_empty": _assay_specific_files_are_empty(metadata),
         "missing_assay_specific_files": _missing_assay_specific_files(metadata),
         "is_upload_directory_empty": _is_upload_directory_empty(metadata),
         "is_dataset_directory_empty": _is_dataset_directory_empty(metadata),
         "is_doi_org_url": _is_doi_org_url(metadata),
-        "is_contributors_metadata_file_empty": _is_contributors_metadata_file_empty(
-            metadata
-        ),
-        "missing_contributors_metadata_file": _missing_contributors_metadata_file(
-            metadata
-        ),
+        "is_contributors_metadata_file_empty": _is_contributors_metadata_file_empty(metadata),
+        "missing_contributors_metadata_file": _missing_contributors_metadata_file(metadata),
         "missing_instrument_metadata_file": _missing_instrument_metadata_file(metadata),
         "has_registration_metadata": _has_registration_metadata(metadata),
         "has_orcid_contributor_metadata": _has_orcid_contributor_metadata(metadata),
-        "instrument_metadata_file_is_empty": _instrument_metadata_file_is_empty(
-            metadata
-        ),
+        "instrument_metadata_file_is_empty": _instrument_metadata_file_is_empty(metadata),
         "has_doi_url": _has_doi_url(metadata),
         "has_empty_directories": _has_empty_directories(metadata),
     }
