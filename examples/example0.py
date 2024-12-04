00dfce4e63f82b73569fa7a3c24c46c9efb76fbb
icaoberg <icaoberg@andrew.cmu.edu>
2024-12-04 13:28:19 -0500

    Update example0.py

--- examples/example0.py
+++ examples/example0.py
@@ -1,10 +1,17 @@
 from datetime import datetime
 from pathlib import Path
 import hubmapbags
+from os import getenv
+from sys import exit
 
-token = ""
+token = getenv("TOKEN")
+if token is None:
+    print("Error: TOKEN environment variable is not set")
+    exit(1)
 
 hubmap_id = "HBM759.JWGJ.636"
+
+# path to your local copy of the inventory directory
 inventory_directory = "/Users/icaoberg/Documents/submission/inventory"
 
 hubmapbags.magic.do_it(
