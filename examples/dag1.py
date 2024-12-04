9d6e64624803dd1c7dcc07da6454903b2820b9ec
icaoberg <icaoberg@andrew.cmu.edu>
2024-12-04 13:26:38 -0500

    Update dag1.py

--- examples/dag1.py
+++ examples/dag1.py
@@ -1,6 +1,11 @@
 import hubmapbags
+from os import getenv
+from sys import exit
 
-token = ""
+token = getenv("TOKEN")
+if token is None:
+    print("Error: TOKEN environment variable is not set")
+    exit(1)
 
 hubmapbags.utilities.clean()
 hubmapbags.reports.daily(token=token)
